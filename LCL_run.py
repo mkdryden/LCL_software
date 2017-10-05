552347d88bef6bb35d820428f56c3ef7a730d693
Harrison Edwards <harrison.edwards@mail.utoronto.ca>
2017-10-05 10:42:08 -0400

    added logging and screen capture

--- LCL_run.py
+++ LCL_run.py
@@ -1,4 +1,4 @@
-import sys
+import sys,logging,os
 import cv2
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -6,6 +6,7 @@ from LCL_ui import Ui_MainWindow
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
+from utils import screen_shooter,now,comment
 
 class ShowVideo(QtCore.QObject):
  
@@ -13,35 +14,28 @@ class ShowVideo(QtCore.QObject):
 	camera_port = 0
 	camera = cv2.VideoCapture(camera_port)
 	VideoSignal = QtCore.pyqtSignal(QtGui.QImage)
- 
+	screenshot_signal = QtCore.pyqtSignal('PyQt_PyObject')
+
 	def __init__(self, parent = None):
 		super(ShowVideo, self).__init__(parent)
-		
+		self.run_video = True
 
 	@QtCore.pyqtSlot()
 	def startVideo(self):		
-		self.run_video = True
-		print('starting video')
+		comment('starting video')
 		while self.run_video:
 			ret, image = self.camera.read()
- 
-			color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
- 
-			height, width, _ = color_swapped_image.shape
- 
+			self.screenshot_signal.emit(image)
+			color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 			
+			height, width, _ = color_swapped_image.shape 
 			qt_image = QtGui.QImage(color_swapped_image.data,
 									width,
 									height,
 									color_swapped_image.strides[0],
-									QtGui.QImage.Format_RGB888)
- 
-			self.VideoSignal.emit(qt_image)
+									QtGui.QImage.Format_RGB888) 
+			self.VideoSignal.emit(qt_image)			
 			QApplication.processEvents()
-	
-	@QtCore.pyqtSlot()
-	def stop_video(self):
-		print('stopping video')
-		self.run_video = False
+
 
 class ImageViewer(QtWidgets.QWidget):
 	def __init__(self, parent = None):
@@ -53,29 +47,30 @@ class ImageViewer(QtWidgets.QWidget):
 		painter = QtGui.QPainter(self)
 		painter.drawImage(0,0, self.image)
 		self.image = QtGui.QImage()
-
  
 	@QtCore.pyqtSlot(QtGui.QImage)
 	def setImage(self, image):
 		if image.isNull():
-			print("Viewer Dropped frame!")
- 
-		self.image = image.scaled(221,171)
-		# if image.size() != self.size():
-		# 	self.setFixedSize(image.size())
+			comment("Viewer Dropped frame!")		
+		self.image = self.resize_dynamically(image)
 		self.update()
+	
+	def resize_dynamically(self,image):
+		return image.scaled(window.ui.verticalLayoutWidget.size())
 
 class main_window(QMainWindow):
 	def __init__(self):
 		super(main_window, self).__init__()
 
 		# set up the video classes and thread
-		self.thread = QtCore.QThread()
-		self.thread.start()
 		self.vid = ShowVideo()
-		self.vid.moveToThread(self.thread)
+		# self.vid.moveToThread(self.thread)
+
 		self.image_viewer = ImageViewer()
 		self.vid.VideoSignal.connect(self.image_viewer.setImage)
+
+		self.screen_shooter = screen_shooter()
+		self.vid.screenshot_signal.connect(self.screen_shooter.screenshot_slot)
 
 		# Set up the user interface from Designer.
 		self.ui = Ui_MainWindow()
@@ -85,20 +80,18 @@ class main_window(QMainWindow):
 		self.ui.verticalLayout.addWidget(self.image_viewer)
 
 		# Connect up the buttons.
-		self.ui.left_button.clicked.connect(self.print_test)
-		self.ui.right_button.clicked.connect(self.vid.startVideo)
-		self.ui.down_button.clicked.connect(self.vid.stop_video)
-		print('finished init')
+		# TODO add comment logging from the user
+		# TODO add general screenshot button so user can comment on what is seen
+		self.ui.target_screenshot_button.clicked.connect(self.screen_shooter.save_target_image)			
+		self.ui.non_target_screenshot_button.clicked.connect(self.screen_shooter.save_non_target_image)			
+		
+		self.show()
+		comment('finished init')
 
-	def print_test(self):
-		print('test')
 
-
-app = QApplication(sys.argv)
-#############
-
-#############
-window = main_window()
-window.show()
-print('exit with code: ' + str(app.exec_()))
-sys.exit()
+if __name__ == '__main__':	
+	app = QApplication(sys.argv)
+	window = main_window()
+	window.vid.startVideo()
+	comment('exit with code: ' + str(app.exec_()))
+	sys.exit()
---
+++ utils.py
@@ -0,0 +1,50 @@
+from PyQt5 import QtCore
+import time,os,datetime
+import cv2
+import logging
+
+def now():
+	return datetime.datetime.now().strftime('%d_%m_%Y___%H.%M.%S.%f')
+
+def comment(text):
+	'''
+	prints to screen and logs simultaneously
+	'''
+	now_time = now()
+	logging.info('{0}{1}{2}'.format(text,
+		'.'*(80-(len(text)+len(now_time))),
+		now_time))
+	print(text)
+
+class screen_shooter():
+	'''
+	handles the various different types of screenshots
+	'''
+	
+	@QtCore.pyqtSlot('PyQt_PyObject')
+	def screenshot_slot(self,image):
+		self.image = image
+
+	@QtCore.pyqtSlot()
+	def save_target_image(self):		
+		comment('taking target picture')
+		cv2.imwrite(os.path.join(experiment_folder_location,
+			'target___{}.jpg'.format(now())),self.image)
+	
+	@QtCore.pyqtSlot()
+	def save_non_target_image(self):
+		comment('taking non target picture')
+		cv2.imwrite(os.path.join(experiment_folder_location,
+			'non_target___{}.jpg'.format(now())),self.image)
+
+	@QtCore.pyqtSlot()
+	def save_misc_image(self):
+		comment('taking miscellaneous picture')
+		cv2.imwrite(os.path.join(experiment_folder_location,
+			'miscellaneous___{}.jpg'.format(now())),self.image)
+
+experiment_name = 'experiment_{}'.format(now())
+experiment_folder_location = os.path.join(os.path.dirname(os.path.abspath(__file__)),'Experiments',experiment_name) 
+os.makedirs(experiment_folder_location)
+logging.basicConfig(filename=os.path.join(experiment_folder_location,
+	'{}.log'.format(experiment_name)), level=logging.INFO)
